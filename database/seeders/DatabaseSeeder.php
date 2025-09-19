961868308da1ba0630315102ae61594693307a23
Galeano Santiago <galeanosantiago10@gmail.com>
2025-09-19 18:02:04 -0300

    seeders y database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,11 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'role' => 'admin',
+            'password' => bcrypt('12345678'),
         ]);
+        
     }
 }
